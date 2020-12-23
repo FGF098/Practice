39119b576fb4055de0da23c0fac61363024a1114
FGF098 <bazhenov.095@gmail.com>
2020-12-24 00:06:54 +0200

    Realized system programs

--- systemprograms.cpp
+++ systemprograms.cpp
@@ -2,5 +2,29 @@
 
 SystemPrograms::SystemPrograms()
 {
+    // TODO:
+}
 
+SystemPrograms::SystemPrograms(const QVector<Program>* newPrograms)
+{
+    int size = newPrograms->size();
+
+    for(int i = 0; i < size; i++)
+    {
+        programs.push_back((*newPrograms)[i]);
+    }
+}
+
+QVector<const Program*> SystemPrograms::getPrograms()
+{
+    int size = programs.size();
+
+    QVector<const Program*> result(size);
+
+    for(int i = 0; i < size; i++)
+    {
+        result[i] = &(programs[i]);
+    }
+
+    return result;
 }
--- systemprograms.h
+++ systemprograms.h
@@ -4,16 +4,24 @@
 #include <QVector>
 #include <QPair>
 
-#include "systemresourses.h"
 #include "program.h"
 
 // class for containing programs in system
-class SystemPrograms : SystemResourses
+class SystemPrograms
 {
 public:
 
+    // add default programs
+    SystemPrograms();
+
+    // create with custom programs
+    SystemPrograms(const QVector<Program>* newPrograms);
+
+    QVector<const Program*> getPrograms();
+
 protected:
 
+    QVector<Program> programs = QVector<Program>();
 };
 
 #endif // SYSTEMPROGRAMS_H
